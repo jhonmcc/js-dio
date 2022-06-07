3bf68ceb2588083303e9a7f01f578601b31e868e
jhonmcc <jhonmcc@gmail.com>
2022-06-07 17:52:05 -0300

    tipos de variaveis

---
+++ javascript/variaveis.js
@@ -0,0 +1,9 @@
+var a = 1; //variavel escopo global
+
+let b = 1; //variavel escopo local
+
+//padrao de declaracao das variaveis
+var camelCase = 'a';
+
+//constantes padrao SNAKE_UPPER_CASE, escopo de bloco, não faz hoisting 
+const DAYS_WEEK = 7;
