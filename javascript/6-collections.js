f4ddac758edbcae6f1a146711ea004bdfd60ec61
jhonmcc <jhonmcc@gmail.com>
2022-06-13 20:05:19 -0300

    exemplo collectioin adicionado

---
+++ javascript/6-collections.js
@@ -0,0 +1,19 @@
+// maps chave valor
+var exMap = new Map();
+
+exMap.set('apple', 'fruit');
+exMap.get(apple);
+exMap.delete(apple);
+
+print(exMap);
+
+// sets > colecao de valores unicos
+var arr = [1,2,3,4,5]
+var exSet = new Set(arr);
+
+console.log(exSet);
+exSet.add(6);
+exSet.has(6);
+console.log(exSet);
+exSet.delete(6);
+console.log(exSet);
